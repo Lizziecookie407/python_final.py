356353336d83f74bf45869f263b26b40aca2c120
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-09-25 19:43:16 +0000

    hangman

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+# KR EH AT KS 6th Team Game Hangman
+import random
+
+#Variables
+words= ["father", "ocean", "water", "kenji", "ducky", "kayze", "eliza", "smoke","train","layer"]#list of five letter words
+word = random.choice(words) #the chosen word that th will be trying to guess
+#output= this w.choice()e a list of the spaces and letters that we'll print
+#user_input = input("Guess a letter").strip().lower
+print(word)
+space = "_"
+
+print(f"{space} {space} {space} {space} {space}")
+
+
 
