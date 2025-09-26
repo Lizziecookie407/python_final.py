89037e1d6836dcfcdb59fade60cf554d63081357
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-09-26 19:45:26 +0000

    Co-authored-by: ducks4444 <ducks4444@users.noreply.github.com> Co-authored-by: KenGi07 <KenGi07@users.noreply.github.com> Co-authored-by: Refrigeratorz17 <Refrigeratorz17@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,14 +2,36 @@
 import random
 
 #Variables
-words= ["father", "ocean", "water", "kenji", "ducky", "kayze", "eliza", "smoke","train","layer"]#list of five letter words
-word = random.choice(words) #the chosen word that th will be trying to guess
-#output= this w.choice()e a list of the spaces and letters that we'll print
-#user_input = input("Guess a letter").strip().lower
+words= ["father", "ocean", "water", "kenji", "ducky", "kayze", "eliza", "smoke","train","layer"]
+word = random.choice(words) 
+
 print(word)
 space = "_"
 
-print(f"{space} {space} {space} {space} {space}")
+print("Hi welcome to hangman!\nHow the game consists is we will have a five letter word that will remain unknown, we will have the user guess letters to try and put the word together, once the player guesses a letter it will appear in the order it is in the word, you will have 6 strikes")
+
+#print(f"{space} {space} {space} {space} {space}")
 
 
+#if word == user_input:
+#    print("You guessed the word!")
+#else:
+#    print("Guess again.")
 
+def guess():
+    user_input = input("Guess a letter: ").strip().lower()
+    for letter in word:
+        if user_input == letter:
+            print(f"it matches")
+            break
+        elif user_input != letter:
+            print("that is a strike")
+        else:
+            print("this is the else statement")
+
+
+guess()
+guess()
+guess()
+guess()
+guess()
