3fa45440a04075dc325b9a6acfc49c652fbc1f42
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-09-26 19:47:14 +0000

    fix

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ word = random.choice(words)
 print(word)
 space = "_"
 
-print(f"{space} {space} {space} {space} {space}")
+print("Hi welcome to hangman!\nHow the game consists is we will have a five letter word that will remain unknown, we will have the user guess letters to try and put the word together, once the player guesses a letter it will appear in the order it is in the word, you will have 6 strikes")
+
+#print(f"{space} {space} {space} {space} {space}")
+
+
 
 
 #if word == user_input:
