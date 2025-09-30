0c2d5c1bb513c6390cd5d0afdc62b140015eb930
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-09-30 19:44:06 +0000

    save Co-authored-by: ducks4444 <ducks4444@users.noreply.github.com> Co-authored-by: KenGi07 <KenGi07@users.noreply.github.com> Co-authored-by: Refrigeratorz17 <Refrigeratorz17@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,16 +3,15 @@
 import random
 
 #Kenji and Adelle
-words= ["father","ocean","water","kenji","ducky","kayze","eliza","smoke","train", "layer","alone","brain","craft","paint","flute","beach","heart","human","image","lemon","lucky","mouse","metal","noise","plane","phone","power","quiet","ready","store","sport","storm","today","table","under","video","watch","young"] 
-word = random.choice(words)
+words= ["ocean","water","kenji","ducky","kayze","eliza","smoke","train", "layer","alone","brain","craft","paint","flute","beach","heart","human","image","lemon","lucky","mouse","metal","noise","plane","phone","power","quiet","ready","store","sport","storm","today","table","under","video","watch","young", "slate", "crane", "brick", "slate", "stare", "raise", "arise", "farts", "bread",] 
+word = random.choice(words) 
 
 #Kayzee
-#print(word)
 space = "_"
 strikes = 0
 
 #Adelle
-print("Hi welcome to hangman!\nThis game consists of an unkown five letter word. First, begin to assemble the word by guessing one letter at a time. Second, if you guess a letter that is not in the word, a strike will appear. If you gain 6 strikes, you're out. Lastly, guess letters to try to complete the word with minimal strikes. Have fun playing!")
+print("Hi welcome to hangman!\nThis game consists of an unkown five letter word. First, begin to assemble the word by guessing one letter at a time. Second, if you guess a letter that is not in the word, a strike will appear. If you gain 8 strikes, you're out. Lastly, if you think you know the word, you can guess the whole word at once. Try to fill in the whole word with minimal strikes. Have fun playing!")
 
 guessed_letters = []
 output_list = []
@@ -20,8 +19,17 @@ output_list = []
 #Eliza and Kayzee
 def guess():
     user_input = input("Guess a letter: ").strip().lower()
-    if user_input in guessed_letters:
+    if user_input == "":
+        print("Please enter a valid letter.")
+        return 0
+    elif user_input in guessed_letters:
         print("You already guessed that letter!")
+        return 0
+    elif user_input == word:
+        exit("Horray! You guessed the word! Rerun the code to play again.")
+        return 0
+    elif len(user_input) > 1:
+        print("Incorrect. Only guess one letter.")
         return 0
     else:
         guessed_letters.append(user_input)
@@ -33,7 +41,7 @@ def guess():
         return 1
 
 #Eliza
-while strikes < 6:
+while strikes < 8:
     strikes += guess()
     print(f"Strikes: {strikes}")
     for letter in word:
@@ -51,6 +59,9 @@ while strikes < 6:
         continue
 
 #Kenji
-while strikes == 6:
-    print("You lost! Rerun the code to play again.")
-    strikes += 1
+def you_lost():
+    print(f"You lost! The word was {word}.")
+    return strikes + 1
+
+if strikes == 8:
+    you_lost()
